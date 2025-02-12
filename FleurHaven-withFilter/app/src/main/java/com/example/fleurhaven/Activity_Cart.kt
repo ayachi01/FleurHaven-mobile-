cab405e3b4802a6b18784d984541c0071e6a2343
zorro <150763793+bianca-gabriel0@users.noreply.github.com>
2025-02-12 15:32:31 +0800

    update cart

--- FleurHaven-withFilter/app/src/main/java/com/example/fleurhaven/Activity_Cart.kt
+++ FleurHaven-withFilter/app/src/main/java/com/example/fleurhaven/Activity_Cart.kt
@@ -1,31 +1,35 @@
 package com.example.fleurhaven
 
+import android.annotation.SuppressLint
 import android.content.Intent
-import android.os.Bundle
-import android.widget.ImageButton
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
+import com.example.fleurhaven.R
+
 
 class Activity_Cart : AppCompatActivity() {
-
-    private lateinit var cart_icon: ImageButton
-    private lateinit var profile_icon: ImageButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cart)
 
 
-        // Find the cart icon button
-        this.profile_icon = findViewById(R.id.profile_icon)
-        profile_icon.setOnClickListener {
-            // Start Activity_Cart when cart icon is clicked
-            val intent2 = Intent(this, Activity_Main::class.java)
-            startActivity(intent2)
+        val cartIcon = findViewById<ImageButton>(R.id.cart_icon)
+        val profileIcon = findViewById<ImageButton>(R.id.profile_icon)
+
+
+        cartIcon.setOnClickListener {
+            val intent = Intent(this, Activity_Cart::class.java)
+            startActivity(intent)
         }
+
+        profileIcon.setOnClickListener {
+            val intent = Intent(this, Activity_Login::class.java)
+            startActivity(intent)
         }
+
 
     }
-
+}
